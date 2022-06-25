f886f952944512ecb65132127750ef2e8a0987e2
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2022-06-24 17:47:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-var i = 0
-let text = "Yes or No";
-let confirm = window.confirm(text)
-if (confirm == true) {
-  while (i < 100) {
-    var num1 = Math.floor(Math.random() * 1920);
-    var num2 = Math.floor(Math.random() * 1080);
-    var popUp = window.open('https://Pop-Ups.rockgamerak.repl.co/popup.html', '', 'width=100,height=125')
-    popUp.moveTo(num1, num2);
-    i++;
-    popUp.close()
-  }
-}
 let hiddenDiv = document.querySelector('[place]')
 let content = hiddenDiv.innerHTML
 
@@ -20,7 +7,13 @@ let i = 0
 function addContent() {
   if (i < times) {
     hiddenDiv.innerHTML = hiddenDiv.innerHTML + content
+    var num1 = Math.floor(Math.random() * 1920);
+    var num2 = Math.floor(Math.random() * 1080);
+    var popUp = window.open('https://Pop-Ups.rockgamerak.repl.co/popup.html', '', 'width=100,height=125')
+    popUp.moveTo(num1, num2);
+    i++;
+    popUp.close()
   }
 }
 
-setInterval(addContent, 1000);
+setInterval(addContent, 1000)
